85d6a2fbfb9790918108c3228de45a34539a653d
Emily Teixeira <emilyteixeira.student@careerdevs.com>
2018-12-28 19:45:15 +0000

    first commit

---
+++ function.js
@@ -0,0 +1,19 @@
+//normal function statement
+function sayHi() {
+    console.log('hi');
+}
+
+sayHi();
+
+//
+function callFunction(fun){
+    fun();
+}
+
+//function expression
+var sayBye = function(){
+    console.log('bye');
+};
+
+callFunction(sayBye);
+
