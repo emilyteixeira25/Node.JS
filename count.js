5b18eb4c75dffd7b58185fd4269a262663350102
Emily Teixeira <emilyteixeira.student@careerdevs.com>
2018-12-28 20:04:55 +0000

    module, export

--- count.js
+++ count.js
@@ -2,5 +2,43 @@ var counter = function(arr){
     return 'There are ' + arr.length + ' elements in this array';
 };
 
-module.exports = counter;
+var adder = function(a,b){
+    return `The sum of the 2 numbers is ${a+b}`;
+};
 
+var pi = 3.142;
+
+module.exports.counter = counter;
+module.exports.adder = adder;
+module.exports.pi = counter;
+
+// // another way
+
+// module.exports.counter = function(arr){
+//     return 'There are ' + arr.length + ' elements in this array';
+// };
+
+// module.exports.adder = function(a,b){
+//     return `The sum of the 2 numbers is ${a+b}`;
+// };
+// module.exports.pi = 3.142;
+
+
+
+//another way
+
+// var counter = function(arr){
+//     return 'There are ' + arr.length + ' elements in this array';
+// };
+
+// var adder = function(a,b){
+//     return `The sum of the 2 numbers is ${a+b}`;
+// };
+
+// var pi = 3.142;
+
+// module.exports =  {
+//     counter: counter,
+//     adder: adder,
+//     pi: pi
+// }
